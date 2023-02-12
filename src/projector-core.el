bf8e09d445075f7ac2a9e590caa28c03b18616d4
Mohsin Kaleem <mohkale@kisara.moe>
2023-02-12 10:22:36 +0000

    project: Add GPL/v3 LICENSE

--- src/projector-core.el
+++ src/projector-core.el
@@ -1,5 +1,20 @@
 ;;; projector-core.el --- Core library for `projector' -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2023  Mohsin Kaleem
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; Core functions for usage of `projector' including project registration, value
