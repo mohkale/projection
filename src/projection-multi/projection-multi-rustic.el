b17811d6ff448939a7859cf19652b254864ad7b5
Mohsin Kaleem <mohkale@kisara.moe>
2023-07-26 20:56:28 +0100

    multi-rustic: Add triggers for when rustic is installed

---
+++ src/projection-multi/projection-multi-rustic.el
@@ -0,0 +1,85 @@
+;;; projection-multi-rustic.el --- Projection integration for `compile-multi' and the rustic projects. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Mohsin Kaleem
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This library exposes a target generation function for `compile-multi' which
+;; generates available targets from the `rustic' package when it is installed.
+;; This simply provides convenient access to helper functions from `rustic'
+;; through `compile-multi'.
+
+;;; Code:
+
+(require 'cl-lib)
+
+(require 'projection-multi)
+(require 'projection-types)
+
+;;;###autoload
+(defun projection-multi-rustic-targets (&optional project-type)
+  "`compile-multi' target generator function for `rustic' projects.
+When set the generated targets will be prefixed with PROJECT-TYPE."
+  (setq project-type (or project-type "rustic"))
+
+  (when (featurep 'rustic)
+    (cl-loop
+     for (command . label) in
+     `((rustic-compile . "compile")
+       (rustic-cargo-add . "cargo:add")
+       (rustic-cargo-rm . "cargo:remove")
+       (rustic-cargo-upgrade . "cargo:upgrade")
+       (rustic-cargo-fmt . "cargo:format")
+       (rustic-cargo-fmt . "format:project")
+       (rustic-cargo-run . "cargo:run")
+       (rustic-cargo-comint-run . "cargo:run:comint")
+       (rustic-cargo-plain-run . "cargo:run:plain")
+       (rustic-cargo-outdated . "cargo:outdated")
+       (rustic-cargo-expand . "cargo:macro-expand")
+       (rustic-cargo-bench . "cargo:bench")
+       (rustic-cargo-build-doc . "cargo:doc:build")
+       (rustic-cargo-doc . "cargo:doc:open")
+       (rustic-cargo-lints . "cargo:lints")
+       (rustic-cargo-install . "cargo:install")
+       (rustic-cargo-update . "cargo:update")
+       (rustic-cargo-clippy . "cargo:clippy")
+       (rustic-cargo-clippy . "cargo:clippy:fix")
+       ,@(when (derived-mode-p 'rustic-mode)
+           `((rustic-format-buffer . "format:buffer")
+             (rustic-format-file . "format:file")
+             (rustic-cargo-add-missing-dependencies . "cargo:add-deps")
+             ,@(when (region-active-p)
+                 `((rustic-format-region . "format:region"))))))
+     collect (cons (concat project-type ":" label) command))))
+
+;;;###autoload
+(defun projection-multi-compile-rustic ()
+  "`compile-multi' wrapper for only `rustic' targets."
+  (interactive)
+  (projection-multi-compile--run
+   (projection--current-project 'no-error)
+   `((t ,#'projection-multi-rustic-targets))))
+
+;;;###autoload
+(with-eval-after-load 'projection-types
+  (oset projection-project-type-rust-cargo compile-multi-targets
+        (seq-uniq
+         (append
+          (oref projection-project-type-rust-cargo compile-multi-targets)
+          (list #'projection-multi-rustic-targets)))))
+
+(provide 'projection-multi-rustic)
+;;; projection-multi-rustic.el ends here
