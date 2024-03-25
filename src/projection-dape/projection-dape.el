68abb9dfab5e85daa31961be10362ca02effeeeb
Mohsin Kaleem <mohkale@kisara.moe>
2024-03-25 19:31:46 +0000

    chore: Missing commentary for projection-dape

--- src/projection-dape/projection-dape.el
+++ src/projection-dape/projection-dape.el
@@ -23,7 +23,10 @@
 
 ;;; Commentary:
 
-;; TODO: commentary.
+;; This package allows selecting debugger targets based on the current project
+;; type and then invoking dape. This can mean extracting the apropriate executable
+;; and argv values for a test or selecting a specific go package to pass to go
+;; delve.
 
 ;;; Code:
 
